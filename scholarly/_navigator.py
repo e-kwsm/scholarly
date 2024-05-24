58f66e4cae97a38c2d93cb03a982886df3aa6884
Eisuke Kawashima <e-kwsm@users.noreply.github.com>
2024-05-24 18:37:50 +0900

    fixup! fixup! fix: fix typing

--- scholarly/_navigator.py
+++ scholarly/_navigator.py
@@ -1,10 +1,10 @@
-from typing import Iterator
-
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 from __future__ import unicode_literals
 from ._proxy_generator import ProxyGenerator, MaxTriesExceededException, DOSException
+
+from typing import Iterator
 
 from bs4 import BeautifulSoup
 
